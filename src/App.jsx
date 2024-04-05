d537e6aa5cae1a8e2703fcde779df20a8b2b94fe
SarthakB07 <sarthak.bansal@spit.ac.in>
2024-04-05 14:49:26 +0530

    api set for app and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "http://localhost:8000/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
